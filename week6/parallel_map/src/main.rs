02487750d2b9a4a204d872b71a5f95f100b6fa49
liuchangliang03 <liuchangliang@liuchangliang03.local>
2021-11-09 11:35:29 +0800

    week6 done

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,47 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    for _ in 0..input_vec.len() {
+        output_vec.push(Default::default());
+    }
+    let (s, r): (
+        crossbeam_channel::Sender<(usize, T)>,
+        crossbeam_channel::Receiver<(usize, T)>,
+    ) = crossbeam_channel::unbounded();
+    let (os, or) = crossbeam_channel::unbounded();
+    for _ in 0..num_threads {
+        let r_ = r.clone();
+        let os_ = os.clone();
+        thread::spawn(move || {
+            loop {
+                match r_.recv() {
+                    Ok(tuple) => {
+                        let tuple_ = (tuple.0, f(tuple.1));
+                        os_.send(tuple_).unwrap();
+                    }
+                    Err(_) => break,
+                }
+            }
+            drop(os_);
+        });
+    }
+    // input_vec.into_iter().enumerate().for_each(|tuple| {
+    //     s.send(tuple).unwrap();
+    // });
+    while input_vec.len() != 0 {
+        let tuple = (input_vec.len() - 1, input_vec.pop().unwrap());
+        s.send(tuple).unwrap();
+    }
+    drop(s);
+    drop(os);
+    loop {
+        match or.recv() {
+            Ok(tuple) => {
+                output_vec[tuple.0] = tuple.1;
+            }
+            Err(_) => break,
+        }
+    }
     output_vec
 }
 
